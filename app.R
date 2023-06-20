e5e03c6473f022be9059511dbab3f5375f60d284
Birkenmaier <Lukas.Birkenmaier@gesis.org>
2023-06-20 17:27:26 +0200

    making list smaller

--- app.R
+++ app.R
@@ -20,27 +20,36 @@ ui <- fluidPage(theme = "flatly",
                 useShinyjs(),
                 use_prompt(),
                 includeCSS("styles.css"),
-                windowTitle = "ValiTex",  # Text displayed in the browser window title
+                windowTitle = "ValiTex Framework",  # Text displayed in the browser window title
                 navbarPage(title = "ValiTex",
                            theme = "navbar-default",
                 #Background ----
                 tabPanel("Home",
                          value = "home",
-                         h2("ValiTex- A Validation Framework for Computational Text-based Measures of Social Science Constructs", align = "center"),
+                         h2("ValiTex - A Validation Framework for Computational Text-based Measures of Social Science Constructs", align = "center"),
                          h4("Beta Version 0.9", align = "center"),
                 h3("About"),
                          p("This webpage accompanies the ", tags$a(href="https://github.com/lukasbirki/ValiTex-Checklist",target="_blank", 
                         "ValiTex"),"framework by", tags$a(href="https://github.com/lukasbirki/ValiTex-Checklist",target="_blank", 
                                                           "Birkenmaier et al. (2023)."),
-                  "The framework is designed to help reserachers that aim to measure social science constructs using computational text analysis to validate thier measures."),
+                  "The framework is designed to help reserachers that aim to measure social science constructs using computational text analysis to validate their empirical measures."),
                 h3("Overview"),
-                                p("Computational methods to analyse textual data require careful validation (Grimmer and Stewart, 2013; Grimmer et al., 2022). However, social science researchers
-                  often lack common terminology and a unified framework that provides guidance to do so. We present a novel validation framework for text analysis (ValiTex) that guides scholars who aim to measure social science constructs based on textual data. Conceptuall, the framework concists of two components:"),
-                tags$ul(type = "A",
+                                p("Validation is a critical tasks in text analysis and natuaral language processing. 
+                               As its core, validation requires different activities to demonstrate that a method measures what it purports to measure. 
+                               Recently, there has been a skyrocketing of studies using computational text analysis to measure social science construct from text, 
+                               such as sexism, polulist language, or ideology. 
+                               However, validing these measures are challenging, especially because textual data can often be interpreted differently."),
+                              p("One crucial problem in the validation of text-based measures is the lack of concepual clarity of how to conduct validation. 
+                              This espresses itself, among others, in the absence of commonly-shared terminology and general framework that provides guidance to do so. 
+                                To guide researchers who aim to measure social science constructs based on textual data, we therefore present a novel validation framework for text analysis called ValiTex. 
+                                Conceptuall, the framework concists of two components:"),
+                tags$ul(
                   tags$li(tags$b("Conceptual Model:"),"General structure along distinct phases on how to approach validation"),
-                  tags$li(tags$b("Dynamic Checklist:"),"A list of concrete validation steps for each phase"),),
-                p("For a detailed introduction into the theoretical background of the framework, please refer to the paper [Link to preprint] or click on the respective component below"),
-                
+                  tags$li(tags$b("Dynamic Checklist:"),"A list of concrete validation steps for each phase of the conceptual model"),),
+                p("If you want to learn more about the component of ValiTex, please click on the respective 
+                  section below or have a look at our",tags$a(href="https://github.com/lukasbirki/ValiTex-Checklist",target="_blank", 
+                                                                "Working Paper")),
+      
                 tags$style(
                   HTML(".tabset { width: 80%; }")
                 ),
@@ -49,25 +58,61 @@ ui <- fluidPage(theme = "flatly",
                     title = "Conceptual Model",
                     value = "r",
                     h3("Conceptual Model"),
-                    p("The conceptual model defines three major phases which researchers should follow when validating computational text-based measures. These Phases are"),
+                    p("The conceptual model defines three major phases which researchers should follow when validating computational text-based measures:"),
                     tags$ul(
-                    tags$li(p(tags$code("Substantive Phase:"), "Demonstrate the theoretical underpinning of the measures")),
-                    tags$li(p(tags$code("Structural Phase:"), "Evaluate the characteristics of the textual model and its output")),
-                    tags$li(p(tags$code("External Phase:"), "Test how the measures relates to unrelated measures or information")),
-                    
-                    
-                      ),
-                    
+                    tags$li(p(tags$b(tags$code("Substantive Phase:")), "The substantive phase comprises the",tags$b(" theoretical underpinning")," of the measure.
+                              It entails all validation steps which ensure the adequacy of the measurement process regarding the operationalization of the construct. 
+                              Exemplary validation steps are",tags$i("Justifying the link between the measures and the construct")," or ",tags$i("Justifying the choice of preprocessing decisions."))),
+                    tags$li(p(tags$b(tags$code("Structural Phase:")), "The structural phase includes the evaluation of the",tags$b("properties of the model and its measures"),
+                    "It entails all validation steps to carefully examine and evaluate the characteristics of the textual model and its output and to identify  biases or errors.
+                    Exemplary validation steps are ",tags$i("Feature Importance Analysis"), "or", tags$i("Error Analysis"))),
+                    tags$li(p(tags$b(tags$code("External Phase:")), "The external phase comprises the",tags$b("relationship with unrelated information."),
+                              "It entails all validation steps that tests for how the measures relate to to independent measures or exogenous events. 
+                              Exemplary validation steps are ",tags$i("Comparing measures with human-annotated test set"), "or", tags$i("Comparing measures with alternative text-based methods"))),),
                     div(
                       tags$img(src = "framework.png", id = "myimage", style = "margin-top: 40px;margin-bottom: 40px; cursor: pointer;"),
-                      p(class = "caption", "Figure 1: Conceptual Model (click to expand)"))
+                      p(class = "caption", "Figure 1: Conceptual Model (click to expand)")),
                     ),
                   tabPanel(
                     title = "Checklist",
                     value = "r",
                     h3("Checklist"),
-                    p("The dynamic checklist within ValiTex defines and characterizes empirical validation steps, organized based on overarching validity dimensions. It provides a comprehensive list of validation steps with descriptions, evaluation instructions, and references to supplementary literature."),
-                    div(tags$img(src = "checklist.png", id = "myimage", style = "margin-top: 40px;margin-bottom: 40px; cursor: pointer;"),
+                    p("The dynamic checklist then provides a comprehensive list of validation steps for each phase within the structural model. 
+                    Each validation step comes with a set of information, such as",
+                    
+                    tags$table(  style = "border-collapse: separate; border-spacing: 11px;",
+
+                      tags$tr(
+                        tags$th("Variable"),
+                        tags$th("Information")
+                      ),
+                      tags$tr(
+                        tags$td(tags$code("Status")),
+                        tags$td("Relevance of the validaton step (either recommended or optional)")
+                      ),
+                      tags$tr(
+                        tags$td(tags$code("Considerations")),
+                        tags$td("Detailed description of the validation step")
+                      ),
+                      tags$tr(
+                        tags$td(tags$code("Reasoning")),
+                        tags$td("Information on how to evaluate the validation step ")
+                      ),
+                      tags$tr(
+                        tags$td(tags$code("Dimension")),
+                        tags$td("The type of validation step as defined in the conceptual model")
+                      ),
+
+                      tags$tr(
+                        tags$td(tags$code("References")),
+                        tags$td("Reference to further literature and information")
+                      )
+                    )),
+                    p("To use the checklist in your research just click on",tags$b("Get Started")," in the navigation bar. 
+                      There, you can generate your own checklist depending on the type of text method used. Furthermore, you will be able to 
+                      download a Word or PDF template which you can fill out by your own and attach to your publication."),
+                    div(tags$img(src = "checklist.png", id = "myimagecheck", style = "margin-top: 40px;margin-bottom: 40px; cursor: pointer;"),
+
                         p(class = "caption", "Figure 2: Screenshot Checklist (click to expand)"))
                     ,
                   )),
@@ -77,27 +122,26 @@ ui <- fluidPage(theme = "flatly",
                         p("This work is licensed under a",
                           a("Creative Commons Attribution-NonCommercial 4.0 International License (CC BY-NC 4.0)",
                             href = "https://creativecommons.org/licenses/by-nc/4.0/",
-                            target = "_blank"))))),
+                            target = "_blank")))))
                 
                ),
                 #Checklist----
-                tabPanel("Checklist",
-                         h3("User Instructions", add_prompt(
-                           tags$span(
-                             class = "question-button",
-                             "?"), 
-                           position = "bottom", message = "For more information, see xx and y"
-                         ),),
+                tabPanel("Get Started",
+                         h3("User Instructions"),
                          p(HTML(paste("This application generates an adaptable checklist that you can use to validate your text-based measures. 
                 Each row within the table corresponds to one validation step (i.e., a single reported and clearly demarcated validation activity). Validation steps can be either ", 
                                       span(style="color:#ed969e; font-weight: bold", "recommended "), "or ", 
                                       span(style="color:#96caed; font-weight: bold", "optional "),"depending on their relevance.", sep = ""),"As outlined in the corresponding paper, researchers should initially follow the order of the phases, starting with the substantive validation steps and ending with external validation steps while continuously considering robustness checks. 
                 However, researchers might adapt this process to their individual use case."),
                            p("ValiTex accounts for differences in validation practices across text-based methods and research contexts. At present, ValiTex differentiates between four broad types of text-based methods:"),
-                           tags$ul(tags$li(tags$b("Dictionary:"),"Rule-based methods that include words or phrases along with their respective meanings or sentiments (e.g., SentiWS)"),
-                                   tags$li(tags$b("Supervised:"),"Machine-learning methods that include some form of training data and test set and/or classification task (e.g., SVM, BERT)"),
-                                   tags$li(tags$b("Unsupervised (Topic Model):"),"Unsupervised methods that generate topis based on word-coocurrences (e.g., LDA) "),
-                                   tags$li(tags$b("Unsupervised (Text Scaling)"),"Unsupervised methods that produce scores that relate to some underlying scale based on word-coocurrences (e.g., Wordfish, Wordscores)"),
+                           tags$ul(tags$li(tags$b("Dictionary:"),"Rule-based methods that include words or phrases along with their respective meanings or sentiments",add_prompt(tags$span(class = "question-button", "?"), 
+                                                                                                                                                                                  position = "bottom", message = "E.g., SentiWS, LIWC")),
+                                   tags$li(tags$b("Supervised:"),"Machine-learning methods that include some form of training data and test set and classification task",add_prompt(tags$span(class = "question-button", "?"), 
+                                                                                                                                                                                       position = "bottom", message = "E.g., Logistic regressiion, CNN, BERT")),
+                                   tags$li(tags$b("Unsupervised (Topic Model):"),"Unsupervised methods that generate topis based on word-coocurrences",add_prompt(tags$span(class = "question-button", "?"), 
+                                                                                                                                                                  position = "bottom", message = "E.g., Latent Dirichlet Allocation (LDA)")),
+                                   tags$li(tags$b("Unsupervised (Text Scaling)"),"Unsupervised methods that produce scores that relate to some underlying scale based on word-coocurrences", add_prompt(tags$span(class = "question-button", "?"), 
+                                                                                                                                                                                                        position = "bottom", message = "E.g., Wordfish, Wordscores")),
                            ),
                            tags$p(tags$b("Please start by choosing a method from the drop-down menue below."), "The complete list of validation steps is also available on ",
                                   tags$a(href="https://github.com/lukasbirki/ValiTex-Checklist/tree/main/data",target="_blank", 
@@ -106,7 +150,6 @@ ui <- fluidPage(theme = "flatly",
                            
                            # Sidebar layout with input and output definitions 
                            verticalLayout(
-                             
                              # Sidebar panel for inputs
                              sidebarPanel(width = 12,
                                           
@@ -121,6 +164,7 @@ ui <- fluidPage(theme = "flatly",
                              
                              # Main panel for displaying outputs 
                              mainPanel(width = 12, 
+
                                        tags$script('
   $(document).on("click", "#myimage", function() {
     if ($(this).hasClass("expanded")) {
@@ -131,29 +175,34 @@ ui <- fluidPage(theme = "flatly",
       $(this).css("max-width", "90%");
     }
   });
-'),conditionalPanel(
-  tags$style(HTML('.group {background-color: #ccc !important;}')),
+
+  $(document).on("click", "#myimagecheck", function() {
+    if ($(this).hasClass("expanded")) {
+      $(this).removeClass("expanded");
+      $(this).css("max-width", "30%");
+    } else {
+      $(this).addClass("expanded");
+      $(this).css("max-width", "90%");
+    }
+  })'),
+  conditionalPanel(tags$style(HTML('.group {background-color: #ccc !important;margin: auto;}')),
   condition = "input.Method != '--Please select a method--'",  # Only show table when Method input is not empty
   # downloadButton("downloadData", "Download (Excel)",
   #                style = "float: right; margin-left: 10px;"),
   downloadButton("Download_word", "Download (Word)",
                  style = "float: right; margin-left: 10px;"),
   tags$h4(HTML(paste("Your selected text method is:",tags$b(tags$span(textOutput("value")), style = "display:inline-block;")), sep = "")),
+  DT::dataTableOutput("table", width = "100%")),
   
-  DT::dataTableOutput("table"),  # Change from tableOutput to dataTableOutput
-  
-),
-
-                             )
-                           )
+                             )),
                          ),
-  div(class = "footer",
-      p("Contact:",a(" lukas.birkenmaier@gesis.org", href = "lukas.birkenmaier@gesis.org"),
-        
-        p("This work is licensed under a",
-          a("Creative Commons Attribution-NonCommercial 4.0 International License (CC BY-NC 4.0)",
-            href = "https://creativecommons.org/licenses/by-nc/4.0/",
-            target = "_blank"))))
+div(class = "footer",
+    p("Contact:",a(" lukas.birkenmaier@gesis.org", href = "lukas.birkenmaier@gesis.org"),
+      
+      p("This work is licensed under a",
+        a("Creative Commons Attribution-NonCommercial 4.0 International License (CC BY-NC 4.0)",
+          href = "https://creativecommons.org/licenses/by-nc/4.0/",
+          target = "_blank"))))
                          
                          ),
 #FaQ----
@@ -196,9 +245,9 @@ server <- function(input, output) {
   shinyjs::runjs("$('#heading1').click(function() { $('#content1').toggle(); })")
   output$table <- renderDT({
     df |>
-      dplyr::select(Phase, `Validation Step`, input$Method,Considerations,"Performance Criteria") |>
+      dplyr::select(Phase, `Validation Step`, input$Method,Considerations,"Performance Criteria",Dimension,`Source / References`) |>
       dplyr::filter(!!sym(input$Method) != "n.a.") |>
-      dplyr::rename(Status = input$Method) |>
+      dplyr::rename(Status = input$Method, References = `Source / References`, Reasoning = "Performance Criteria") |>
       dplyr::mutate(Phase = factor(Phase, levels = c("Substantive Phase", "Structural Phase", "External Phase", "Continuous Evaluation: Robustness Checks"))) |> 
       mutate(" " = '<input type="checkbox" name="select_row">', .before = 1) -> df_output
     
@@ -249,7 +298,7 @@ server <- function(input, output) {
   })
   
   
-  # Downloadable word of selected dataset 
+  # Downloadable word files of selected dataset 
   output$Download_word <- downloadHandler(
     filename = function() {
       paste("checklist_validity_",tolower(as.character(input$Method)), ".docx", sep = "")
--- faq.R
+++ faq.R
@@ -2,23 +2,27 @@ df_daq <- data.frame(
   question = c("What is ValiTex?",
                "Why do I need to validate text-based measures?",
                "What is the basis of ValiTex?",
-               "How does ValiTex approach validation?",
                "What are the components of ValiTex?",
                "How is the checklist organized?",
                "Does ValiTex consider different types of text-based measures?",
-               "Are all validation steps equally important?"),
+               "Are all validation steps equally important?",
+               "What are the consequences of omitting certain validation steps?"),
   answer = c("ValiTex is a novel framework designed to validate computational text-based measures of social science constructs. 
-                It provides researchers with tools and guidance to navigate the validation process for text-based research.",
-             "Explanation",
-             "ValiTex builds upon the methodological research in the social sciences, particularly psychometrics, which has a well-established understanding of validation practices. 
-                It also incorporates insights from a recent review of validation practices in text-based research by Birkenmaier et al. (2023).",
-             "ValiTex follows three key principles:\n1. Organizing the validation process across key phases outlined in the psychometric tradition.\n2. Applying different validation steps within each phase, with some steps being more relevant for demonstrating measurement validity.\n3. Including continuous evaluation of the measure's robustness towards researchers' degree of freedom or context-specific factors.",
+                It aims to provide a comprehensive perspective on measurement validation, and to provide practical guidance for researchers who want to validate computational text-based measures of social science constructs.",
+             "Validating text-based measures is essential due to the inherent uncertainty associated with text data. 
+             Textual information is subjective and can be prone to various sources of bias, such as interpretation and misrepresentation. 
+             Without validation, there is a risk of misinterpreting or misclassifying the text, leading to erroneous conclusions and unreliable analyses.",
+             "ValiTex builds upon the methodological research in the social sciences, particularly psychometrics, which has 
+             a well-established understanding of validation practices. It also incorporates insights from a recent review of validation 
+             practices in text-based research by Birkenmaier et al. (2023).",
              "ValiTex consists of two major components: the conceptual model and a dynamic checklist.\n- The conceptual model defines relevant validity dimensions for text-based research and provides a general structure for approaching validation.\n- The dynamic checklist characterizes empirical validation steps, grouped by overarching validity dimensions, for each phase within the conceptual model.",
-             "The checklist includes a list of validation steps along with their descriptions and instructions for evaluation. It may incorporate common cut-off criteria and references to supplementary literature where applicable.",
+             "The checklist includes a list of validation steps along with their descriptions and instructions for evaluation. It further provides reference to supplementary literature.",
              "Yes, ValiTex distinguishes between three commonly used text-based methods: \"dictionaries\" (DI), \"supervised methods\" (SU), and \"unsupervised methods.\" The latter is further divided into \"unsupervised: topic model\" (UST) and \"unsupervised: text scaling\" (USS). Certain validation steps may only be applicable to specific types of measures.",
-             "No, validation steps are classified as either recommended or optional. Recommended steps are central for providing relevant and necessary validation evidence, while optional steps offer additional supporting evidence. Researchers are advised to perform all recommended steps and provide a rationale for intentionally omitting any of them."
+             "No, validation steps are classified as either recommended or optional. Recommended steps are central for providing relevant and necessary validation evidence, while optional steps offer additional supporting evidence. Researchers are advised to perform all recommended steps and provide a rationale for intentionally omitting any of them.",
+             "The consequences of omitting validation steps usually depends on the research context. Generally, it follows that the uncertainty over the true value of the measure increases."
   )
 )
+
 
 
 shinyLink <- function(to, label) {
