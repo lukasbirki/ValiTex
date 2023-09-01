d85cd5dc3f0352bcbacad458693b0c945d75a1a1
Birkenmaier <Lukas.Birkenmaier@gesis.org>
2023-09-01 12:32:37 +0200

    fixing

--- R/framework.R
+++ R/framework.R
@@ -1,11 +1,5 @@
-#' This is data to be included in my package
+#' This is the raw dataset for the ValiTex checklist
 #'
 #' @author Lukas Birkenmaier \email{lukas.birkenmaier@outlook.de}
-#' @format A data frame with x rows and y column(s)
-#' \describe{
-#'   \item{column1}{Describe column here . . .}
-#'   \item{column2}{Describe column here . . .}
-#'  ...
-#' }
 #' @references \url{no reference}
 "framework"
