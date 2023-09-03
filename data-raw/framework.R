d1b87e5221c511db1a51e334284d9b4ae3b76dbd
Birkenmaier <Lukas.Birkenmaier@gesis.org>
2023-09-03 20:55:27 +0200

    updates

--- data-raw/framework.R
+++ data-raw/framework.R
@@ -1,6 +1,9 @@
 ## code to prepare `framework` dataset goes here
 
 framework <- load("data-raw/framework.rda")
+framework2 <- readxl::read_xlsx("data-raw/framework.xlsx")
 #framework <- readxl::read_xlsx("data-raw/framework.xlsx")
 
 usethis::use_data(framework)
+usethis::use_data(framework2)
+
